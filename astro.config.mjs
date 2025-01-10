4d183e3498f54aa57b4a7c08bbcc92ffeb7da9af
Aman Anil <amananiloffical@gmail.com>
2025-01-10 13:20:58 +0530

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,10 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  vite: {
+    ssr: {
+      noExternal: ['react-compiler-runtime']
+    }
+  }
 });
