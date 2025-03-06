fe88badfe86f31480a8cbc466148a80fdf20c2a7
Aman Anil <amananiloffical@gmail.com>
2025-03-06 15:04:39 +0530

    updated sitemap, robots and favicons images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   vite: {
     optimizeDeps: {
       exclude: ['react-compiler-runtime']
     }
   },
-  site: 'https://alcanzaship.com',
+  site: 'http://localhost:4321', // Development URL
   base: '/',
 });
